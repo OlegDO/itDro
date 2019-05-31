015e4c9bce3f40ff4b4a738b78dea50ac3bfc989
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2019-05-31 03:16:40 +0300

    feat: implement general comment function for review component

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../shared/service/auth.service';
+import {generalReviewService} from '../../shared/service/generalReview.service';
 
 @Component({
   selector: 'app-reviews',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reviews.component.css']
 })
 export class ReviewsComponent implements OnInit {
+  user: any = {};
+  reviews: any = {};
+  userComment: string;
+  link = '/reviews';
 
-  constructor() { }
+  constructor(private auth: AuthService, private gReview: generalReviewService) { }
 
   ngOnInit() {
+    this.load();
+  }
+  private postReview() {
+    this.gReview.postGeneralReview(this.user).then((data) => this.getGeneralReview());
   }
 
+  private getGeneralReview() {
+    this.gReview.getGeneralReview(this.link).subscribe((data) => (data));
+  }
+  private load() {
+    this.gReview.getGeneralReview(this.link).subscribe((data) => {
+      this.reviews = data;
+      console.log(this.reviews);
+    });
+  }
+
+  getInfo() {
+    this.auth.user.subscribe((user) => {
+      this.user.userName = user.displayName;
+      this.user.userPhoto = user.photoURL;
+      this.user.email = user.email;
+      this.user.userId = user.uid;
+      if (this.userComment) {
+        this.user.comment = this.userComment;
+      }
+      this.postReview();
+      console.log(this.user);
+    });
+  }
 }
---
+++ src/app/shared/service/generalReview.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class generalReviewService {
+  generalReviewCollection: AngularFirestoreCollection<any>;
+
+  constructor(private db: AngularFirestore) {
+    this.generalReviewCollection = this.db.collection('generalReview');
+  }
+
+  postGeneralReview(data: any) {
+    return this.generalReviewCollection.add(data);
+  }
+
+  getGeneralReview(data: any) {
+    return this.db.collection('generalReview', (ref) => ref).valueChanges();
+  }
+}
