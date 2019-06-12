3291bcf68f7ca3950a406f338e9ac0fea0d3cec8
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2019-06-12 04:18:32 +0300

    docs: remake src of pictures

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBhlPgMiqGz21u2XZ0-nvUlev2hZz3xoPk',
+    authDomain: 'itdro-6a5c5.firebaseapp.com',
+    databaseURL: 'https://itdro-6a5c5.firebaseio.com',
+    projectId: 'itdro-6a5c5',
+    storageBucket: 'itdro-6a5c5.appspot.com',
+    messagingSenderId: '73500779377'
+  }
 };
