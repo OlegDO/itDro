4e33b5196b0fc8fcaff9c7eceab014b2b1f230f3
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2019-06-27 16:22:21 +0300

    docs: add link of project in README

--- src/app/pipes/lessonFilter.pipe.ts
+++ src/app/pipes/lessonFilter.pipe.ts
@@ -3,7 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({
   name: 'lessonFilter'
 })
-export class LessonFilterPipe implements PipeTransform{
+export class LessonFilterPipe implements PipeTransform  {
   transform(lessonList, searchStr: string) {
     if (lessonList.length === 0 || searchStr === '') {
       return lessonList;
